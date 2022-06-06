5890decfad254ca7d309d9e106249628d51b609d
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-06-06 23:50:15 +0530

    Update Q3.cpp

--- day 1 weekly 296/Q3.cpp
+++ day 1 weekly 296/Q3.cpp
@@ -1,3 +1,5 @@
+//Brute force
+
 class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& opr) {
@@ -14,6 +16,30 @@ public:
                 
             }
         }
+        return nums;
+    }
+};
+
+
+//Optimal solution
+
+
+class Solution 
+{
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& opr) 
+    {
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]=i;
+        
+        for(int i=0;i<opr.size();i++)
+        {
+            nums[mp[opr[i][0]]]=opr[i][1];
+            mp[opr[i][1]]=mp[opr[i][0]];
+        }
+        
         return nums;
     }
 };
